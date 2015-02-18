781b26e5c06b0d8533d8c382f26c87a7d9aa3af9
vikas k. thakur <vikas.dumca2012@gmail.com>
2015-02-19 01:04:46 +0530

    Update createIndex_tfidf.py

--- tf-idf/createIndex_tfidf.py
+++ tf-idf/createIndex_tfidf.py
@@ -90,10 +90,10 @@ class CreateIndex:
 
     def getParams(self):
         '''get the parameters stopwords file, collection file, and the output index file'''
-        self.stopwordsFile  = '/filepath/stopwords.dat'
-        self.collectionFile = '/filepath/testCollection.dat'
-        self.indexFile      = '/filepath/indexFile.dat'
-        self.titleIndexFile = '/filepath/indexFiletfidf.dat'
+        self.stopwordsFile  = 'stopwords.dat'
+        self.collectionFile = 'testCollection.dat'
+        self.indexFile      = 'indexFile.dat'
+        self.titleIndexFile = 'indexFiletfidf.dat'
         
 
     def createIndex(self):
